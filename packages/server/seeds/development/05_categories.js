8675598bbaa2e0d316faeb4162f5d70688cdc8d6
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-05-18 17:27:20 +0200

    Add seed data

--- packages/server/seeds/development/05_categories.js
+++ packages/server/seeds/development/05_categories.js
@@ -6,9 +6,10 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('categories').del();
   await knex('categories').insert([
-    { id: 1, name: 'Crops', created_at: knex.fn.now() },
-    { id: 2, name: 'Herbs', created_at: knex.fn.now() },
-    { id: 3, name: 'Spices', created_at: knex.fn.now() },
-    { id: 3, name: 'Vegetables', created_at: knex.fn.now() },
+    { id: 1, name: 'Bud/Flower', created_at: knex.fn.now() },
+    { id: 2, name: 'Fruit/Berry', created_at: knex.fn.now() },
+    { id: 3, name: 'Seed', created_at: knex.fn.now() },
+    { id: 4, name: 'Root/Rhizome/Bark', created_at: knex.fn.now() },
+    { id: 5, name: 'Leaf', created_at: knex.fn.now() },
   ]);
 };
