56ab8025b2df0f8e5312af64559aeb750eb81cf6
chunmeishui <chunmeishui578659@gmail.com>
2022-05-21 14:45:57 +0200

    added the LandingPage SortByPart component

---
+++ packages/client/src/components/sortByComponent/SortBy.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './SortBy.styles.css';
+
+export const SortBy = () => {
+  const [dropDown, setDropDown] = useState(true);
+  const display = () => {
+    dropDown ? setDropDown(false) : setDropDown(true);
+  };
+  return (
+    <>
+      <div className="SortBy_item">
+        <span className="sort_item">SORT BY</span>
+        {/* show differnt triangel */}
+        {dropDown ? (
+          <span
+            className="triangle_down"
+            onClick={() => {
+              display();
+            }}
+            aria-hidden="true"
+          />
+        ) : (
+          <span
+            className="triangle_up"
+            onClick={() => {
+              display();
+            }}
+            aria-hidden="true"
+          />
+        )}
+      </div>
+      {/* dispaly or not dispaly the sort list */}
+      {!dropDown ? (
+        <div className="sort_list">
+          <span className="A_Z">A-Z</span>
+          <span>Lowest price</span>
+          <span>New arrivals</span>
+          <span>Category</span>
+        </div>
+      ) : null}
+    </>
+  );
+};
---
+++ packages/client/src/components/sortByComponent/SortBy.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SortBy } from './SortBy';
+
+export default {
+  title: 'SortBy/SortBy',
+  component: SortBy,
+};
+export const SortBya = () => <SortBy />;
