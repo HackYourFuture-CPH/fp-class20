624e12e9980fd1b20032333add876f6934e3dbf8
sumanghimire79 <sumanghimire79@yahoo.com>
2022-05-23 01:01:48 +0200

    Add the dummy-pages v2: unrelated folder,components and files removed

--- packages/client/src/containers/ContactUsPage/ContactUsPage.Container.js
+++ packages/client/src/containers/ContactUsPage/ContactUsPage.Container.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './ContactUsPage.Style.css';
-import { ContactForm } from '../../components/form/contactForm/ContactForm.component.js';
 
 export const ContactUsPage = () => {
-  return (
-    <div className="ContactUsPageContainer">
-      <ContactForm />
-    </div>
-  );
+  return <div className="ContactUsPageContainer" />;
 };
