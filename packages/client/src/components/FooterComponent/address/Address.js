56bdc8171b9621ab28f65e4610aaaee31dc009ab
chunmeishui <chunmeishui578659@gmail.com>
2022-05-23 17:10:16 +0200

    chenged the adress component to text

--- packages/client/src/components/FooterComponent/address/Address.js
+++ packages/client/src/components/FooterComponent/address/Address.js
@@ -1,35 +1,21 @@
 import React from 'react';
 import './Address.styles.css';
-import simplySpices from '../../../../public/assets/SimplySpices.png';
+// import simplySpices from '../../../../public/assets/SimplySpices.png';
 import map from '../../../../public/assets/mapLogo.png';
 
 export const Address = () => {
   return (
     <div className="Address_info">
-      <div>
-        <img
-          src={simplySpices}
-          alt="Canvas Logo"
-          className="logoOfSimplySpices"
-        />
-      </div>
-
-      {/* forth div botom part */}
-      <div className="mapIn">
-        {/* map logo and adress left map logo */}
-        <div>
-          <img className="smallMapIcon" src={map} alt="Canvas map Logo" />
-        </div>
-        {/* map logo and adress right part adress */}
-        <div>
-          <p className="adress">
-            Norrebrogade 666
-            <br />
-            2200 Norrebro
-            <br />
-            Copenhagen
-          </p>
-        </div>
+      <span className="simplySpices">Simply Spices</span>
+      <div className="adress_text_logo">
+        <img className="smallMapIcon" src={map} alt="Canvas map Logo" />
+        <p className="adress">
+          Norrebrogade 666
+          <br />
+          2200 Norrebro
+          <br />
+          Copenhagen
+        </p>
       </div>
     </div>
   );
