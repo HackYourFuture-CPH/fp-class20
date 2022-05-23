b20bb928222a3699f8561be26c492806e6c3607e
Faheem Hassan <faheemhassan1985@gmail.com>
2022-05-24 00:08:06 +0200

    Add contact us sent feedback

---
+++ packages/client/src/components/ContactUsFeedback/ContactUsFeedback.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { ContactUsFeedback } from './ContactUsFeedback.component';
+
+export default {
+  title: 'contact-us-sent-feedback/ConatactUsFeedback',
+  component: ContactUsFeedback.component,
+};
+
+export const contactUsFeedback = () => <ContactUsFeedback />;
