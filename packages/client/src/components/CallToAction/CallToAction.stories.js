1b696c1d4777d25d2ea97973741a07e73ea93cbb
Faheem Hassan <faheemhassan1985@gmail.com>
2022-05-23 23:32:35 +0200

    Add The CallToAction-About

---
+++ packages/client/src/components/CallToAction/CallToAction.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { CallToAction } from './CallToAction.component';
+
+export default {
+  title: 'Call-to-action/Aboutus',
+  component: CallToAction.component,
+};
+
+export const aboutUs = () => <CallToAction />;
