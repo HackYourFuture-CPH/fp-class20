242389eb03feeac8f4d88c6cc44760474dcb0f64
Faheem Hassan <faheemhassan1985@gmail.com>
2022-05-24 13:40:38 +0200

    Add About us component

---
+++ packages/client/src/components/About-us/AboutUs.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { AboutUs } from './AboutUs.component';
+
+export default {
+  title: 'AboutUs/Aboutus',
+  component: AboutUs.component,
+};
+
+export const aboutUs = () => <AboutUs />;
