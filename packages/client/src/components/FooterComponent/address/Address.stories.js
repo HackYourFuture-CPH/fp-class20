6385ce5ced95953e2cd69b0a5989572dcf5768cd
Negar Eghbali <negar.en@gmail.com>
2022-05-26 17:05:49 +0200

    CLASS20-50 if applied it will remove all irrelevant files from the branch

--- packages/client/src/components/FooterComponent/address/Address.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Address } from './Address';
-
-export default {
-  title: 'FooterComponent/FooterComponent',
-  component: Address,
-};
-export const AddressInfo = () => <Address />;
--- packages/client/src/components/FooterComponent/corporate/Corporate.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Corporate } from './Corporate';
-
-export default {
-  title: 'FooterComponent/FooterComponent',
-  component: Corporate,
-};
-export const CorporateInfo = () => <Corporate />;
