14ee4fcea3ab0e3082d2533f21061d803df1af7c
negaren <88971519+negaren@users.noreply.github.com>
2022-05-27 21:12:53 +0200

    CLASS20-50 if applied it will provide button component (#37)
    ADD TO CART, SUBMIT, EDIT YOUR DETAILS, CONTINUE SHOPPING, SHOP, CHECKOUT, REVIEW ORDER, REVIEW ORDER DETAIL buttons components will be usable with respective type prop.

--- packages/client/src/components/Button/Button.component.js
+++ packages/client/src/components/Button/Button.component.js
@@ -1,24 +1,54 @@
+/* eslint-disable no-mixed-operators */
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.styles.css';
+import { ReactComponent as AddToCardSVGIcon } from '../../../public/assets/icon_shopping_cart_white.svg';
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary
+export const Button = ({
+  type,
+  backgroundColor,
+  label,
+  icon,
+  size,
+  primary,
+  ...props
+}) => {
+  // eslint-disable-next-line no-param-reassign
+  // eslint-disable-next-line no-param-reassign
+  // eslint-disable-next-line no-unused-expressions
+  type === ('addToCart' && 'storybook-button--addToCart') ||
+    ('submit' && 'storybook-button--submit') ||
+    ('submitSecondary' && 'storybook-button--submitSecondary') ||
+    ('shop' && 'storybook-button--shop') ||
+    ('editDetails' && 'storybook-button--editDetails') ||
+    ('continueShopping' && 'storybook-button--continueShopping') ||
+    ('continueShopping' && 'storybook-button--continueShoppingSecondary') ||
+    ('reviewOrderDetails' && 'storybook-button--reviewOrderDetails') ||
+    ('checkout' && 'storybook-button--checkout') ||
+    'storybook-button--reviewOrder';
+
+  let mode = primary
     ? 'storybook-button--primary'
     : 'storybook-button--secondary';
+
+  // eslint-disable-next-line no-return-assign
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' ',
-      )}
+      className={[
+        'storybook-button',
+        `storybook-button--${type}`,
+        `storybook-button--${size}`,
+        type === '' ? mode : (mode = ''),
+      ].join(' ')}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
       {label}
+      {icon}
     </button>
   );
 };
@@ -29,17 +59,25 @@ Button.propTypes = {
    */
   primary: PropTypes.bool,
   /**
+   * Is this the principal call to action on the page?
+   */
+  type: PropTypes.string,
+  /**
    * What background color to use
    */
   backgroundColor: PropTypes.string,
+
   /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(['small', 'medium', 'large', 'story']),
+  /**
   /**
    * Button contents
    */
   label: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/no-typos
+  icon: PropTypes.ReactComponent,
   /**
    * Optional click handler
    */
@@ -48,7 +86,9 @@ Button.propTypes = {
 
 Button.defaultProps = {
   backgroundColor: null,
-  primary: false,
-  size: 'medium',
+  type: '',
   onClick: undefined,
+  primary: false,
+  size: 'story',
+  icon: <AddToCardSVGIcon />,
 };
--- packages/client/src/components/Button/Button.stories.js
+++ packages/client/src/components/Button/Button.stories.js
@@ -9,7 +9,7 @@ export default {
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: 'Example/Button',
+  title: 'components/Button',
   component: Button.component,
 };
 
@@ -17,25 +17,120 @@ export default {
 const Template = (args) => <Button {...args} />;
 
 // 👇 Each story then reuses that template
+export const AddToCart = Template.bind({});
+AddToCart.args = {
+  type: 'addToCart',
+  label: 'ADD TO CART',
+};
+
+export const Submit = Template.bind({});
+Submit.args = {
+  type: 'submit',
+  label: 'SUBMIT',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const SubmitSecondary = Template.bind({});
+SubmitSecondary.args = {
+  type: 'submitSecondary',
+  label: 'SUBMIT',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const Shop = Template.bind({});
+Shop.args = {
+  type: 'shop',
+  label: 'SHOP',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const EditDetails = Template.bind({});
+EditDetails.args = {
+  type: 'editDetails',
+  label: 'EDIT DETAILS',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const ContinueShopping = Template.bind({});
+ContinueShopping.args = {
+  type: 'continueShopping',
+  label: 'CONTINUE SHOPPING',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const ContinueShoppingSecondary = Template.bind({});
+ContinueShoppingSecondary.args = {
+  type: 'continueShoppingSecondary',
+  label: 'CONTINUE SHOPPING',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const ReviewOrderDetails = Template.bind({});
+ReviewOrderDetails.args = {
+  type: 'reviewOrderDetails',
+  label: 'REVIEW ORDER DETAILS',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const ReviewOrder = Template.bind({});
+ReviewOrder.args = {
+  type: 'reviewOrder',
+  label: 'REVIEW ORDER',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const Checkout = Template.bind({});
+Checkout.args = {
+  type: 'checkout',
+  label: 'CHECKOUT',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+// 👇 Each story then reuses that template
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
   label: 'Button',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   label: 'Button',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
   label: 'Button',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
   label: 'Button',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+  label: 'Button',
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  icon: <></>,
 };
