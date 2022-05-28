384851ce45a86942c4d77d7c3d02b5be1c17bd54
mag-da-lenka <magdalenka.ag@gmail.com>
2022-05-28 18:19:27 +0200

    git config core.ignore false TEST; duplicated folders deleted

--- packages/client/src/components/Signin/SignIn.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import SignIn from './SignIn.component';
-
-export default {
-  title: 'Main Header/SignIn',
-  component: SignIn,
-};
-export const signIn = () => <SignIn />;
