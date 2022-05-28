e2e727a12aeaae31ad55bc98fed46e6c223aa2b3
magdalena wojtakowska [voigt] <51802866+mag-da-lenka@users.noreply.github.com>
2022-05-28 18:17:34 +0200

    Delete packages/client/src/components/favourites directory

--- packages/client/src/components/favourites/Favourites.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Favourites from './Favourites.component';
-
-export default {
-  title: 'Main Header/Favourites',
-  component: Favourites,
-};
-export const addToFavourites = () => <Favourites />;
