85caf3945e6dd0f96e383bd74d883a30b2bc024a
Mamidi <anant@Anants-MacBook-Pro.local>
2022-05-28 23:22:39 +0200

    bottom nav

---
+++ packages/client/src/components/BottomNavigation/BottomNavigation.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { BottomNavigation } from './BottomNavigation.component';
+
+export default {
+  title: 'BottomNavigation/BottomNavigation',
+  component: BottomNavigation.component,
+};
+export const BottomNav = () => <BottomNavigation />;
