4713278027dd884b7900f9d437b7b3d0b0634e7e
mag-da-lenka <magdalenka.ag@gmail.com>
2022-05-28 18:10:34 +0200

    git config core.ignore false TEST

---
+++ packages/client/src/components/Signin/SignIn.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import signIn from '../../../public/assets/sign_in_image.png';
+
+import './Signin.styles.css';
+
+const SignIn = () => {
+  return (
+    <div className="sign-in-container">
+      <img className="sign-in-icon" src={signIn} alt="signin-icon" />
+      <span className="sign-in-text"> Sign in</span>
+    </div>
+  );
+};
+export default SignIn;
