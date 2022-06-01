1f9f44f077748579f06908dfd6befed72c655d0b
Swetha <91112898+Mamidi001@users.noreply.github.com>
2022-06-01 14:39:51 +0200

    added top-navigation-bar (#55)
    * added top-navigation-bar
    
    * added Navigation
    
    * added naming in the stories
    
    * added feedback
    
    Co-authored-by: Mamidi <anant@Anants-MacBook-Pro.local>

---
+++ packages/client/src/components/Navigation/Cart/Cart.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './Cart.styles.css';
+
+import CartIcon from '../../../../public/assets/vectors/vector_cart.svg';
+
+const Cart = ({ label }) => {
+  return (
+    <div className="cart-container">
+      <img className="cart-icon" src={CartIcon} alt="cart-icon" />
+      <span className="number">0</span>
+      <span className="cart-text">Cart</span>
+    </div>
+  );
+};
+
+export default Cart;
+
+Cart.propTypes = {
+  label: PropTypes.string.isRequired,
+};
