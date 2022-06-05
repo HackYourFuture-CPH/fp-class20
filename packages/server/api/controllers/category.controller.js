c853e8550732964c3f06ed5c1d07fba0b5427072
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-06-05 13:42:52 +0200

    Chang sortby to query

--- packages/server/api/controllers/category.controller.js
+++ packages/server/api/controllers/category.controller.js
@@ -5,26 +5,26 @@ const getCategories = async () => {
   return knex('Categories').select();
 };
 
-const getProducts = async (category, sortby) => {
-  const productByCategory = knex('Categories')
+const getProducts = async (category, query) => {
+  let productByCategory = knex('Categories')
     .join('Products', 'Categories.id', 'Products.categoryId')
     .where('Categories.id', 'like', `${category}%`);
 
-  if (sortby.sort === 'name') {
-    productByCategory.orderBy('Products.name', 'asc');
+  if (query.sort === 'name') {
+    productByCategory = productByCategory.orderBy('Products.name', 'asc');
   }
 
-  if (sortby.sort === 'lowestPrice') {
-    productByCategory.orderBy('Products.size', 'desc');
+  if (query.sort === 'lowestPrice') {
+    productByCategory = productByCategory.orderBy('Products.size', 'desc');
   }
-  if (sortby.sort === 'newest') {
-    productByCategory.orderBy('Products.createdAt', 'asc');
+  if (query.sort === 'newest') {
+    productByCategory = productByCategory.orderBy('Products.createdAt', 'asc');
   }
   return productByCategory;
 };
 
-const getProductByCategory = async (category, sortby) => {
-  const products = await getProducts(category, sortby);
+const getProductByCategory = async (category, query) => {
+  const products = await getProducts(category, query);
   if (products.length === 0) {
     throw new HttpError('No product under this category', 404);
   }
