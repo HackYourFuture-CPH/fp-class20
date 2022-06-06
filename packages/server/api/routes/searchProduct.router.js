4de8f843891b3f7eb070e9462bdfabe1ced96a88
Negar Eghbali <negar.en@gmail.com>
2022-06-06 21:37:20 +0200

    CLASS20-64 swagger documentation is added

--- packages/server/api/routes/searchProduct.router.js
+++ packages/server/api/routes/searchProduct.router.js
@@ -4,6 +4,33 @@ const router = express.Router({ mergeParams: true });
 
 const searchProduct = require('../controllers/searchProduct.controller');
 
+/**
+ * @swagger
+ * /searchProduct?name={name}:
+ *  get:
+ *    tags:
+ *    - search product
+ *    summary: Search for products by partial name
+ *    description:
+ *      Will search for products with names that includes the searched phrase.
+ *    produces: application/json
+ *    parameters:
+ *     - in: query
+ *       name: name
+ *       schema:
+ *         type: string
+ *         description: Partial name of the product
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ *      404:
+ *        description: the searched item does no exist.
+ *
+ */
+
 router.get('/', (req, res, next) => {
   searchProduct
     .getSearchedProducts(req.query.name)
