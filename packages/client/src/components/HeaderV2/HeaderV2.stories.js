ff88adb753327e70012c8f36c3047996f4617898
chunmeishui <chunmeishui578659@gmail.com>
2022-06-06 19:32:15 +0200

    changed the name of the file

--- packages/client/src/components/HeaderV2/HeaderV2.stories.js
+++ packages/client/src/components/HeaderV2/HeaderV2.stories.js
@@ -2,15 +2,8 @@ import React from 'react';
 import HeaderV2 from './HeaderV2.component';
 
 export default {
-  title: 'HeaderV2/header2',
+  title: 'Header/header',
   component: HeaderV2.component,
 };
 
-// 👇 We create a “template”
-export const HeaderV2Ab = () => <HeaderV2 />;
-// 👇 Each story then reuses that template
-// export const Primary = HeaderV2Ab.bind({});
-// Primary.args = {
-//   primary: true,
-//   label: 'Spice',
-// };
+export const Header = () => <HeaderV2 />;
