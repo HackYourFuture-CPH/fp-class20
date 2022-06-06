3851a60c240c9a4b2c0d5171747660f1d173342c
chunmeishui <chunmeishui578659@gmail.com>
2022-06-06 19:32:15 +0200

    changed the name of the file

--- packages/client/src/components/HeaderV2/Bottom-navigation/BottomNavigationV2.stories.js
+++ packages/client/src/components/HeaderV2/Bottom-navigation/BottomNavigationV2.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BottomNavigationV2 } from './BottomNavigationV2.component';
 
 export default {
-  title: 'HeaderV2/BottomNavigation',
+  title: 'Header/header',
   component: BottomNavigationV2.component,
 };
-export const BottomNav = () => <BottomNavigationV2 />;
+export const RightBottomNav = () => <BottomNavigationV2 />;
