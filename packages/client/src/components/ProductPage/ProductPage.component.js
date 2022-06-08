af4aae1159ede3a0eef7d5f91fa18edd8fc3659d
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-06-08 15:07:02 +0200

    start fixing the skeleton of the product page

--- packages/client/src/components/ProductPage/ProductPage.component.js
+++ packages/client/src/components/ProductPage/ProductPage.component.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Carousel from '../Carousel/Carousel.component';
 import { AddProductToCart } from '../ProductComponent/AddProductToCart.component';
+import { Footer } from '../Footer/Footer.component';
 
 export const ProductPage = () => {
+  const [id, setProductId] = useState(null);
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch(`api/products/${id}`)
+      .then((response) => response.json())
+      .then((productTodisplay) => setProduct(productTodisplay));
+  }, [id]);
   return (
     <div>
-      <AddProductToCart />
+      <AddProductToCart product={product} />
+      <Carousel />
+      <Footer />
     </div>
   );
 };
--- packages/client/src/components/ProductPage/ProductPage.stories.js
+++ packages/client/src/components/ProductPage/ProductPage.stories.js
@@ -1,34 +1,12 @@
-// import React from 'react';
-// import { ProductPage } from './ProductPage.component';
-// import Image from '../../../public/assets/images/spices_square/dried_juniper_berries.jpeg';
+import React from 'react';
+import { ProductPage } from './ProductPage.component';
 
-// export default {
-//   title: 'components/Product',
-//   component: ProductPage.component,
-// };
+export default {
+  title: 'components/Product',
+  component: ProductPage.component,
+};
 
-// const products = [
-//   {
-//     id: 1,
-//     size: '250g glass jar',
-//     price: 350,
-//     name: 'Juniper Berries',
-//     description: 'This is the description of juniper berries',
-//     pictureUrl: Image,
-//   },
-// ];
-// // export const Template = () => <ProductContainer />;
-
-// const Template = (args) => <ProductPage {...args} />;
-
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   // eslint-disable-next-line object-shorthand
-//   product: products[0],
-//   addToCartOnClick: () => {
-//     // eslint-disable-next-line no-console
-//     console.log('Add to click');
-//   },
-//   saveToFavorites: 'save to favorites',
-//   closeModal: () => {},
-// };
+// export const Template = () => <ProductContainer />;
+const Template = (args) => <ProductPage {...args} />;
+export const Primary = Template.bind({});
+Primary.args = {};
