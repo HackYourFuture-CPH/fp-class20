f58aac88e8dfe97dafb2fbace0ea660a8b2158b4
chunmeishui <chunmeishui578659@gmail.com>
2022-06-13 18:32:05 +0200

    changed dump page to smart page with props

--- packages/client/src/containers/CategoryPage/CategoryPage.Container.js
+++ packages/client/src/containers/CategoryPage/CategoryPage.Container.js
@@ -3,11 +3,19 @@ import LandingSortBy from './LandingPageSortByPart/LandingSortBy.component';
 import Footer from '../../components/Footer/Footer.component';
 import './CategoryPage.Style.css';
 
+const textObj = {
+  sideBar: 'Simply Spices / Spices by plant part / Berries and Fruit',
+  main: 'Berries and Fruit',
+};
+
 export const CategoryPage = () => {
   return (
     <div className="category-page">
       <div className="sort-by-background">
-        <LandingSortBy className="landing-sort-by-component-info select  text-big text-small" />
+        <LandingSortBy
+          className="landing-sort-by-component-info select  text-big text-small"
+          textObj={textObj}
+        />
       </div>
       <div className="category-middle-main-background">
         <div className="category-middle-main-container">
--- packages/client/src/containers/CategoryPage/LandingPageSortByPart/LandingSortBy.component.js
+++ packages/client/src/containers/CategoryPage/LandingPageSortByPart/LandingSortBy.component.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './LandingSortBy.styles.css';
 import SortBy from './SortByComponent/SortBy.component';
 
-const LandingSortBy = () => {
+const LandingSortBy = (textObj) => {
   return (
     <div className="landing-sort-by-component-info">
-      <span className="text-samll">
-        Simply Spices / Spices by plant part / Berries and Fruit
-      </span>
-      <span className="text-big">Berries and Fruit</span>
+      <span className="text-samll">{textObj.textObj.sideBar}</span>
+      <span className="text-big">{textObj.textObj.main}</span>
       <div className="sort-by-container">
         <SortBy />
       </div>
