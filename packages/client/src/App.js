ec7915bf9bcabb9aec296ddb564bdfd6b18e866a
chunmeishui <chunmeishui578659@gmail.com>
2022-06-14 19:27:48 +0200

    added Favourite page draft version

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AboutUsPage } from './containers/AboutUsPage/AboutUsPage.Container';
 import { CategoryPage } from './containers/CategoryPage/CategoryPage.Container';
 import { ContactUsPage } from './containers/ContactUsPage/ContactUsPage.Container';
+import { FavouritePage } from './containers/FavouritePage/FavouritePage.Container';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 import { ProductPage } from './containers/ProductPage/ProductPage.Container';
@@ -19,6 +20,7 @@ function App() {
           <Route path="/contact-us" element={<ContactUsPage />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/*" element={<PageNotFound />} />
+          <Route path="/favourites" element={<FavouritePage />} />
         </Routes>
       </Router>
     </div>
