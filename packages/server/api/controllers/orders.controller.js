f59a36f8f0dcf898ac10b050410e23bd152979d7
hwaleedali <hwaleedalibutt@gmail.com>
2022-06-19 00:35:14 +0200

    endpoints for order

--- packages/server/api/controllers/orders.controller.js
+++ packages/server/api/controllers/orders.controller.js
@@ -1,19 +1,16 @@
 const knex = require('../../config/db');
 
 const getOrders = async () => {
-  return knex('Orders')
+  const orders = await knex('Orders')
     .join('Users', 'users.id', 'Orders.userId')
     .join('OrderItems', 'OrderItems.orderId', 'Orders.id')
     .join('Products', 'Products.id', 'OrderItems.productId');
+  return orders;
 };
 
 const postOrders = async (request) => {
   const orderPost = await knex('orders').insert(request);
-  return {
-    code: 201,
-    status: 'success ',
-    message: `Created ${orderPost}`,
-  };
+  return orderPost;
 };
 
 const getOrderByID = async (id) => {
@@ -21,20 +18,22 @@ const getOrderByID = async (id) => {
     return 'id must contain number';
   }
   const orderByID = await knex('orders').where('id', Number(id));
-  if (orderByID.length === 0) {
-    return {
-      code: '404',
-      status: 'failed',
-      error: 'order ID not found',
-      message: 'Invalid ID',
-    };
-  }
+  // if (orderByID.length === 0) {
+  //   return {
+  //     code: '404',
+  //     status: 'failed',
+  //     error: 'order ID not found',
+  //     message: 'Invalid ID',
+  //   };
+  // }
   return orderByID;
 };
 
 const deleteOrders = async (reqId, body) => {
-  const orders = await knex('Orders').where({ productId: reqId }).delete(body);
-  return orders;
+  const ordersdel = await knex('Orders')
+    .where({ productId: reqId })
+    .delete(body);
+  return ordersdel;
 };
 
 module.exports = {
