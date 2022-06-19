59ac1e5091d16fe6f3ce7602541564ff33785a3b
Faheem Hassan <faheemhassan1985@gmail.com>
2022-06-19 15:52:25 +0200

    deleted nav and footer

--- packages/client/src/containers/PageNotFound/PageNotFound.Container.js
+++ packages/client/src/containers/PageNotFound/PageNotFound.Container.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './PageNotFound.Style.css';
-import { Footer } from '../../components/Footer/Footer.component';
-import Navigation from '../../components/Navigation/Navigation.component';
 import PageNotFound from '../../components/PageNotFound/PageNoFound.component';
 
 export function PageNotFoundPage() {
   return (
     <div className="page-not-found-container">
-      <Navigation className="toggle-button-bottom" />
       <PageNotFound />
-      <Footer className="pagenotfound-text-big" />
     </div>
   );
 }
