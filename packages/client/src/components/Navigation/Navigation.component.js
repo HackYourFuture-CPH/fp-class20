274437449b5e957304fe80bfa2d97bc951b80a97
chunmeishui <chunmeishui578659@gmail.com>
2022-06-19 15:44:49 +0200

    added the props to the navigation

--- packages/client/src/components/Navigation/Navigation.component.js
+++ packages/client/src/components/Navigation/Navigation.component.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import TopNavBar from '../TopNavBar/TopNavBar.component';
+import PropTypes from 'prop-types';
 import BottomNavBar from '../BottomNavBar/BottomNavBar.component';
 
-function Navigation() {
+function Navigation(props) {
   return (
     <div>
       <TopNavBar />
-      <BottomNavBar />
+      <BottomNavBar
+        setSortProduct={props.setSortProduct}
+        setBreadcrumbs={props.setBreadcrumbs}
+      />
     </div>
   );
 }
 
 export default Navigation;
+
+Navigation.propTypes = {
+  setSortProduct: PropTypes.func,
+  setBreadcrumbs: PropTypes.func,
+};
+
+Navigation.defaultProps = {
+  setSortProduct: () => {},
+  setBreadcrumbs: () => {},
+};
