3bd89b9f0f973fa99018d9a0ff96f0dad6e88daa
chunmeishui <chunmeishui578659@gmail.com>
2022-06-19 15:58:14 +0200

    added category page

--- packages/client/src/containers/CategoryPage/CategoryPage.Container.js
+++ packages/client/src/containers/CategoryPage/CategoryPage.Container.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Footer from '../../components/Footer/Footer.component';
 import './CategoryPage.Style.css';
+import SortBy from '../../components/SortBy/SortBy.component';
+import Navigation from '../../components/Navigation/Navigation.component';
+import { ProductCard } from '../../components/ProductComponent/ProductCard.component';
+import Preloader from '../../components/Preloader/Preloader.component';
+
+const textObj = {
+  sidebar: 'Simply Spices / All Spices',
+  main: 'All Spices',
+};
 
 export const CategoryPage = () => {
-  return (
-    <div className="category-page-container">
-      <span>Category Page</span>
+  const [breadCrumbs, setBreadcrumbs] = useState(textObj);
+  const [products, setProducts] = useState([]);
+  const [sortProduct, setSortProduct] = useState('/products');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    categoryProducts();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortProduct]);
+
+  const categoryProducts = async () => {
+    try {
+      setLoading(true);
+      const dataJson = await fetch(`http://localhost:5000/api${sortProduct}`);
+
+      const productsCategory = await dataJson.json();
+      setProducts(productsCategory);
+      setLoading(false);
+      // eslint-disable-next-line no-console
+      console.log(products);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+
+  const productItem = products.map((product) => (
+    <div className="favorite-item" key={product.name}>
+      <ProductCard product={product} variant="small" />
     </div>
+  ));
+  return (
+    <>
+      <Navigation
+        className="toggle-button-bottom"
+        setSortProduct={setSortProduct}
+        sortProduct={sortProduct}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+      <SortBy
+        textObj={breadCrumbs}
+        products={products}
+        setProducts={setProducts}
+      />
+
+      {loading ? (
+        <Preloader loading={loading} />
+      ) : (
+        <div className="category-page-main-container">
+          <div className="category-middle-main">{productItem}</div>
+        </div>
+      )}
+
+      <Footer className="text-big" />
+    </>
   );
 };
