9ba7fc532c81e03dcb0333097b99c3864ae6163c
sumanghimire79 <sumanghimire79@yahoo.com>
2022-06-19 12:14:19 +0200

    login with firbase v1

---
+++ packages/client/src/components/Login/Login.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Login } from './Login.component';
+
+export default {
+  title: 'components/Login',
+  component: Login,
+};
+
+export const login = () => <Login />;
