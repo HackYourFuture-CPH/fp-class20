c132abf045bf314dcab75bf3940e13be319eaf2d
chunmeishui <chunmeishui578659@gmail.com>
2022-06-19 15:32:21 +0200

    testing

--- packages/client/src/components/Navigation/Navigation.component.js
+++ packages/client/src/components/Navigation/Navigation.component.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TopNavBar from '../TopNavBar/TopNavBar.component';
 import BottomNavBar from '../BottomNavBar/BottomNavBar.component';
 
+// testing
+
 function Navigation() {
   return (
     <div>
