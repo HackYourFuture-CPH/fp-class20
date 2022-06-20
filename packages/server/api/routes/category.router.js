f2e5b22909822467ffa03816b04da1a2f6779087
qaissaada <qais.m.saada@gmail.com>
2022-06-20 13:11:27 +0200

    add category by id endpoint

--- packages/server/api/routes/category.router.js
+++ packages/server/api/routes/category.router.js
@@ -68,4 +68,43 @@ router.get('/:category', (req, res, next) => {
     .catch(next);
 });
 
+/**
+ * @swagger
+ * /category/{ID}:
+ *  get:
+ *    tags:
+ *    - category
+ *    summary: Get products by category ID.
+ *    description: Will return all products with a matching category ID.
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: ID
+ *       schema:
+ *         type: string
+ *         required: true
+ *         description: The ID of the categoryto get.
+ *     - in: query
+ *       name: sort
+ *       type: string
+ *       enum:
+ *       - newest
+ *       - lowestPrice
+ *       - name
+ *       default: available
+ *       collectionFormat: multi
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+
+router.get('/:id', (req, res, next) => {
+  controller
+    .getCategoryById(req.params.id)
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
 module.exports = router;
