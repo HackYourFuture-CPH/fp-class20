7476afb16ec2638f481a23b204c967218d198ab0
sumanghimire79 <sumanghimire79@yahoo.com>
2022-06-20 09:31:42 +0200

    committed just to checkout to another branch...

---
+++ packages/client/src/components/Form/SigninForm/Signin.component.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
+import './Login.styles.css';
+
+import { auth } from '../../firebase.config';
+
+export function Signin() {
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const register = async () => {
+    try {
+      const userr = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword,
+      );
+      // eslint-disable-next-line no-console
+      console.log(userr);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error.message);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const userrr = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword,
+      );
+      // eslint-disable-next-line no-console
+      console.log(userrr);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error.message);
+    }
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <div className="login-component">
+      <div className="register">
+        <h3> Register User </h3>
+
+        {/* 
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}
+        />
+       */}
+
+        <button type="button" onClick={register}>
+          {' '}
+          Create User
+        </button>
+      </div>
+
+      <div className="login">
+        <h3> Login </h3>
+        <input
+          placeholder="Email..."
+          onChange={(event) => {
+            setLoginEmail(event.target.value);
+          }}
+        />
+        <input
+          placeholder="Password..."
+          onChange={(event) => {
+            setLoginPassword(event.target.value);
+          }}
+        />
+
+        <button type="button" onClick={login}>
+          Login
+        </button>
+      </div>
+      <div className="logout">
+        <h4> User Logged In: </h4>
+        {user?.email}
+
+        <button type="button" onClick={logout}>
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ packages/client/src/components/Form/SigninForm/Signin.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Signin } from './Signin.component';
+
+export default {
+  title: 'components/Login',
+  component: Signin,
+};
+
+export const SigninForm = () => <Signin />;
