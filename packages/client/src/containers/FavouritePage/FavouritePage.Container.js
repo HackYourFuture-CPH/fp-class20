3b07b5607341d966c1a3a476cf24fa45c1313545
chunmeishui <chunmeishui578659@gmail.com>
2022-06-20 16:07:18 +0200

    delete the navigation and footer

--- packages/client/src/containers/FavouritePage/FavouritePage.Container.js
+++ packages/client/src/containers/FavouritePage/FavouritePage.Container.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './FavouritePage.Style.css';
-import Navigation from '../../components/Navigation/Navigation.component';
 import SortBy from '../../components/SortBy/SortBy.component';
-import { Footer } from '../../components/Footer/Footer.component';
 import { ProductCard } from '../../components/ProductComponent/ProductCard.component';
 
 const textObj = { sidebar: 'Simply Spices / Favourites', main: 'Favourites' };
@@ -34,12 +32,10 @@ export const FavouritePage = () => {
   ));
   return (
     <>
-      <Navigation className="toggle-button-bottom" />
       <SortBy textObj={textObj} />
       <div className="favourite-page-container">
         <div className="favourite-page-container-main">{favouriteItem}</div>
       </div>
-      <Footer className="text-big" />
     </>
   );
 };
