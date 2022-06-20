92e865954b51d2140e0b7dc32b49c8a916b28802
Negar Eghbali <negar.en@gmail.com>
2022-06-20 23:04:51 +0200

    CLASS20-72 fetch url is updated to use util function to get the domain

--- packages/client/src/containers/ProductPage/ProductPage.Container.js
+++ packages/client/src/containers/ProductPage/ProductPage.Container.js
@@ -3,6 +3,7 @@ import './ProductPage.Style.css';
 import { useParams } from 'react-router-dom';
 import { ProductCard } from '../../components/ProductComponent/ProductCard.component';
 import getApiBaseUrl from '../../utils/getApiBaseUrl';
+import { SimilarProducts } from './SimilarProducts';
 
 export const ProductPage = () => {
   const { id = 3 } = useParams();
@@ -25,24 +26,6 @@ export const ProductPage = () => {
       .catch((err) => setError(err));
   }, [id]);
 
-  // useEffect(() => {
-  //   fetch(`${getApiBaseUrl()}/api/products/${id}`)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong');
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((productTodisplay) => {
-  //       setProduct(productTodisplay[0]);
-  //       setLoading(false);
-  //     })
-  //     .catch((err) => setError(err));
-  // }, [id]);
-
-  // eslint-disable-next-line no-console
-  console.log(product);
-
   if (isLoading) {
     return <div className="App">Loading...</div>;
   }
@@ -57,6 +40,7 @@ export const ProductPage = () => {
             <div className="product-title">{product.name}</div>
           </div>
           <ProductCard product={product} className="product-card-container" />
+          <SimilarProducts product={product} />
         </>
       )}
     </div>
--- packages/client/src/containers/ProductPage/SimilarProducts.js
+++ packages/client/src/containers/ProductPage/SimilarProducts.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './ProductPage.Style.css';
 import Carousel from '../../components/Carousel/Carousel.component';
-import { ProductCard } from '../../components/ProductComponent/ProductCard.component';
 import getApiBaseUrl from '../../utils/getApiBaseUrl';
 
 export const SimilarProducts = ({ product }) => {
@@ -12,14 +11,14 @@ export const SimilarProducts = ({ product }) => {
   // const [categories, setCategories] = useState([]);
   // const [isCategoryLoading, setCategoryLoading] = useState(true);
 
-  const fetchUrl = async () => {
-    const result = await fetch(
-      `${getApiBaseUrl()}/api/category/${product.categoryId}`,
-    );
-    return result;
-  };
-
   useEffect(() => {
+    const fetchUrl = async () => {
+      const result = await fetch(
+        `${getApiBaseUrl()}/api/products?category=${product.categoryId}`,
+      );
+      return result;
+    };
+
     fetchUrl()
       .then((response) => {
         if (!response.ok) {
@@ -32,51 +31,25 @@ export const SimilarProducts = ({ product }) => {
         setisSimilarProductLoading(false);
       })
       .catch((err) => setError(err));
-  });
-
-  // useEffect(() => {
-  //   fetch(`${getApiBaseUrl()}/api/category/${product.categoryId}`)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong');
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setSimilarProducts(data);
-  //       setisSimilarProductLoading(false);
-  //     })
-  //     .catch((err) => setError(err));
-  // }, [product.categoryId, similarProducts]);
+  }, [product.categoryId]);
 
   if (isSimilarProductLoading) {
     return <div className="App">Loading...</div>;
   }
-  return (
-    <>
-      <div className="product-info-main-page">
-        <div className="product-category">{product.categoryName}</div>
-        <div className="product-title">{product.name}</div>
-      </div>
-      {error ? (
-        <p>{error}</p>
-      ) : (
-        <>
-          <ProductCard product={product} className="product-card-container" />
-          {similarProducts.length !== 1 && (
-            <>
-              <h1 className="title-similar-products">Similar products:</h1>
-              <Carousel
-                key={product.id}
-                items={similarProducts.filter((item) => item.id !== product.id)}
-                show={3}
-                className="carousel"
-              />
-            </>
-          )}
-        </>
-      )}
-    </>
+  return error ? (
+    <p>{error}</p>
+  ) : (
+    similarProducts.length !== 1 && (
+      <>
+        <h1 className="title-similar-products">Similar products:</h1>
+        <Carousel
+          key={product.id}
+          items={similarProducts.filter((item) => item.id !== product.id)}
+          show={3}
+          className="carousel"
+        />
+      </>
+    )
   );
 };
 
