7c07f60630e7eccb6878d64f3eb896db77517a54
sumanghimire79 <sumanghimire79@yahoo.com>
2022-06-21 11:11:28 +0200

    Add the global css

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AboutUsPage } from './containers/AboutUsPage/AboutUsPage.Container';
 import { CategoryPage } from './containers/CategoryPage/CategoryPage.Container';
