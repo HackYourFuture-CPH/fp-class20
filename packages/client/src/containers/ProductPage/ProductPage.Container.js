276e0822d414a1a2be0cd3f64ea01e6840f4e5c5
negaren <88971519+negaren@users.noreply.github.com>
2022-06-21 14:55:58 +0200

    Class20 72 product page container (#80)
    * to resolve the rebase conflict
    
    * CLASS20-72 product page container draft
    
    * CLASS20-72 product page container draft
    
    * CLASS20-72 product category is fetched
    
    * CLASS20-87 fetching promises undefined is resolved by loading condition
    
    * CLASS20-72 imported components names are modified after receiving new merges on develop
    
    * CLASS20-72 styling for responsive page is modified
    
    * CLASS20-72 proptypes revised, page modified to be responsive
    
    * CLASS20-72 proptypes revised, similar products code is provided in a separate component
    
    * CLASS20-72 error handling is added and product on carousel modified to be clickable
    
    * CLASS20-72 modified to pass the checks
    
    * CLASS20-72 modified to pass the checks
    
    * CLASS20-72 styling is modified, similar product header will render only if there is
     similar products
    
    * CLASS20-72 styling is modified
    
    * CLASS20-72 git draft modification
    
    * CLASS20-72 fetch url is updated to use util function to get the domain
    
    * CLASS20-72 comments are deleted
    
    * CLASS20-72 modified based on reviews

--- packages/client/src/containers/ProductPage/ProductPage.Container.js
+++ packages/client/src/containers/ProductPage/ProductPage.Container.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import { ProductCard } from '../../components/ProductComponent/ProductCard.component';
+import React, { useEffect, useState } from 'react';
 import './ProductPage.Style.css';
+import { useParams } from 'react-router-dom';
+import { ProductCard } from '../../components/ProductComponent/ProductCard.component';
+import getApiBaseUrl from '../../utils/getApiBaseUrl';
+import { SimilarProducts } from './SimilarProducts';
 
 export const ProductPage = () => {
-  const product = {
-    id: 1,
-    size: '250g glass jar',
-    price: 350,
-    name: 'Juniper Berries  ',
-    description:
-      'Adored by chefs for their clear, sharp scent, juniper berries are essential for marinades and stewing. Add them to your cold gin and tonic. Enjoy !',
-    pictureUrl: '../assets/images/spices_square/dried_juniper_berries.jpeg',
-  };
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${getApiBaseUrl()}/api/products/${id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('something went wrong');
+        }
+        return response.json();
+      })
+      .then((productTodisplay) => {
+        setProduct(productTodisplay[0]);
+        setLoading(false);
+      })
+      .catch((err) => setError(err));
+  }, [id]);
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
   return (
-    <div className="product-page-container">
-      <span>Product Page </span>
-      <ProductCard product={product} variant="small" />
+    <div className="product-page-main-container">
+      <div className="product-info-main-page">
+        <div className="product-category">{product.categoryName}</div>
+        <div className="product-title">{product.name}</div>
+      </div>
+      <ProductCard product={product} className="product-card-container" />
+      <SimilarProducts product={product} />
     </div>
   );
 };
+
+ProductPage.propTypes = {};
+
+ProductPage.defaultProps = {};
