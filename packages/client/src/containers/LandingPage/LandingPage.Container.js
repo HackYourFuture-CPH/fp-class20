0d8a143d266c5207729e7591dba320cd8b2bcbd4
Vahablotfi <vahablotfi1@gmail.com>
2022-06-22 23:21:16 +0200

    add paginated list of products to landing page

--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Pagination from '../../components/Pagination/Pagination';
+import getApiBaseUrl from '../../utils/getApiBaseUrl';
+import Preloader from '../../components/Preloader/Preloader.component';
 import './LandingPage.Style.css';
+import { ProductsList } from './ProductsList';
 
 export const LandingPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  /* Hard coded number, due to not receiving the total number of products from DB. 
+   https://hackyourfuture-dk.atlassian.net/browse/CLASS20-120?atlOrigin=eyJpIjoiMzYzMTQ0MmFhZDNkNDk1OTgyYjE2M2Y2MTA4YTg2NWEiLCJwIjoiaiJ9
+   */
+
+  const [numberOfPages, setNumberOfPages] = useState(2);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await fetch(
+        `${getApiBaseUrl()}/api/products?pageIndex=${currentPage - 1}`,
+      );
+
+      const fetchedProducts = await response.json();
+
+      setProducts(fetchedProducts);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [currentPage]);
+
+  const changePageByNumber = (num) => {
+    setCurrentPage(num);
+  };
+
+  if (loading) return <Preloader />;
   return (
     <div className="landing-page-container">
-      <span>Landing Page</span>
+      <ProductsList products={products} />
+      <Pagination
+        currentPage={currentPage}
+        pageCount={numberOfPages}
+        onPageChange={changePageByNumber}
+      />
     </div>
   );
 };
