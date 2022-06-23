084f10fd42e986f2f4ca0e5de4fcd9dfdf29af6e
chunmeishui <chunmeishui578659@gmail.com>
2022-06-23 11:31:01 +0200

    added sort by function

--- packages/client/src/containers/FavouritePage/FavouritePage.Container.js
+++ packages/client/src/containers/FavouritePage/FavouritePage.Container.js
@@ -12,7 +12,9 @@ export const FavouritePage = () => {
   const [favourites, setFavourites] = useState([]);
   const [loading, setIsLoading] = useState(false);
   const [isFavorite, setIsFavorite] = useState(true);
-  const { userId } = useParams();
+  const [sort, setSort] = useState('');
+
+  const { user_id: userId } = useParams();
 
   useEffect(() => {
     async function fetchFavourites() {
@@ -32,18 +34,35 @@ export const FavouritePage = () => {
     fetchFavourites();
   }, [userId]);
 
-  const favouriteItems = favourites.map((favourite) => (
-    <ProductCard
-      className="favourite-item"
-      variant="small"
-      product={favourite}
-      isFavorite={isFavorite}
-      setIsFavorite={setIsFavorite}
-    />
-  ));
+  const favouriteItems = favourites
+    // eslint-disable-next-line
+    .sort((a, b) => {
+      if (sort === '') {
+        return 0;
+      }
+      if (sort === 'alphabetically') {
+        return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
+      }
+      if (sort === 'Lowest price') {
+        return Number(a.price) > Number(b.price) ? 1 : -1;
+      }
+      if (sort === 'New arrivals') {
+        return a.createdAt > b.createdAt ? -1 : 1;
+      }
+      return a.name < b.className ? -1 : 1;
+    })
+    .map((favourite) => (
+      <ProductCard
+        className="favourite-item"
+        variant="small"
+        product={favourite}
+        isFavorite={isFavorite}
+        setIsFavorite={setIsFavorite}
+      />
+    ));
   return (
     <>
-      <SortBy textObj={textObj} />
+      <SortBy textObj={textObj} sort={sort} setSort={setSort} />
       <div className="favourite-page-container">
         {loading && <Preloader />}
         <div className="favourite-page-container-main">{favouriteItems}</div>
